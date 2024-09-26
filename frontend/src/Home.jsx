1c8ea61f85612c49441b25ed3b3dc2396a98ef90
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-09-27 00:05:12 +0530

    updating

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import { Link, useParams } from "react-router-dom";
 import { useGetProductsQuery } from "./redux/api/productApiSlice";
-import Loader from "./components/Loader";
-import Header from "./components/Header";
-import { useParams, Link } from "react-router-dom";
+import Message from "./components/Message.jsx"
+import Loader from "./components/Loader.jsx"
+import Header from "./components/Header.jsx"
+import Product from "./pages/Products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
-  const { data, isLoading, isError } = useGetProductsQuery();
+  const { data, isLoading, isError } = useGetProductsQuery({ keyword });
 
-  return <div className=" h-screen bg-[#292B4B] overflow-y-auto">{!keyword ? <Header /> : null}</div>;
+  return (
+    <>
+      {!keyword ? <Header /> : null}
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="danger">
+          {isError?.data.message || isError.error}
+        </Message>
+      ) : (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+              Special Products
+            </h1>
+
+            <Link
+              to="/shop"
+              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+            >
+              Shop
+            </Link>
+          </div>
+
+          <div>
+            <div className="flex justify-center flex-wrap mt-[2rem]">
+              {data.products.map((product) => (
+                <div key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
