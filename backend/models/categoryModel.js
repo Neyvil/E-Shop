02d2d06942db1c3d16d0610e44120708ce039422
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-10-12 00:13:21 +0530

    few changes

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const categorySchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  parentCategory: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    default: null, // If null, this is a top-level category
-  },
-  subcategories: [
-    {
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    parentCategory: {
       type: Schema.Types.ObjectId,
-      ref: 'Category', // Referencing other categories to create a hierarchy
-    }
-  ],
-  gender: {
-    type: String,
-    enum: ["male", "female", "unisex"],
-    default: null, // Optional, only for certain categories
+      ref: "Category",
+      default: null, // If null, this is a top-level category
+    },
+    subcategories: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Category", // Referencing other categories to create a hierarchy
+      },
+    ],
+    gender: {
+      type: String,
+      enum: ["male", "female", "unisex"],
+      default: null, // Optional, only for certain categories
+    },
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Category = mongoose.model("Category", categorySchema);
 
