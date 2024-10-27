7162ef6b79508eb144fe386a5466689f6baa1daf
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-10-27 13:37:04 +0530

    import problem

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from '../middleware/uploadMiddleware.js';
+import upload from '../middlewares/uploadMiddleware.js';
 import {
   createUser,
   loginUser,
