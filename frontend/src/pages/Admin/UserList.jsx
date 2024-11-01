f2a6ef78da7cea4e7024113bb6089103ebec1c5a
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-11-02 01:41:31 +0530

    userlist updated

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useToast } from "../../components/Toast/ToastProvider";
+import AdminMenu from "./AdminMenu.jsx"
 import {
   useDeleteUserMutation,
   useGetUsersQuery,
