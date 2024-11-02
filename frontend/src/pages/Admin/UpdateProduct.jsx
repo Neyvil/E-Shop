0b20f0f0b0970925a4a067865eb68be063c0f7a9
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-11-02 10:59:50 +0530

    updateproduct

--- frontend/src/pages/Admin/UpdateProduct.jsx
+++ frontend/src/pages/Admin/UpdateProduct.jsx
@@ -49,7 +49,7 @@ const UpdateProduct = () => {
       setCategory(productData.category?._id || "");
       setSelectedCategoryPath(productData.category?.path || []);
       if (productData.productImage) {
-        setImagePreview(`https://e-shopbackend-bnov.onrender.com/${productData.productImage.replace(/\\/g, "/")}`);
+        setImagePreview(productData.productImage);
       }
 
       // Handle category-specific fields
